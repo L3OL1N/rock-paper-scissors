563cf93d1e5efd60da0b81df19aae935249e4248
L3OL1N <z3650888@gmail.com>
2022-07-16 16:12:56 +0800

    ADD restart color

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -17,68 +17,83 @@ function playRound(playerSelection, computerSelection) {
     // your code here!
     var PC = computerSelection;
     var player = playerSelection;
-    var roundWinner ="";
-
+    
     if(PC === "rock" && player === "paper" || PC === "paper" && player === "scissors" || PC === "scissors" && player === "rock"){
         PlayerPoint++;
+        winnerColor = "green";
         return roundWinner="You win";
     }
     if(PC === "rock" && player === "scissors" || PC === "paper" && player === "rock" || PC === "scissors" && player === "paper"){
         PcPoint++;
+        winnerColor = "red";
         return roundWinner="You lose";
     }
     else{
+        winnerColor = "white";
         return roundWinner="Draw";
     }
 }
-   
+var winnerColor = "";
+var roundWinner = "";
 var playerSelection = "";
 var computerSelection = computerPlay();
 var PlayerPoint = 0;
 var PcPoint = 0;
-var winner = document.getElementsByClassName("winner");
-console.log(winner[0].innerHTML);
-function game(playerSelection, computerSelection){
-    //console.log("You :"+playerSelection);
-    //console.log("PC :"+computerSelection);
-    playRound(playerSelection, computerSelection);
-    
-    if(PlayerPoint > PcPoint){
-        return roundWinner = "You win";
+var winner = document.getElementsByClassName("winner")[0];
+var pcChoiceText = document.getElementById("PCchoice");
+
+function game(){
+    if(PlayerPoint >= 5){
+        btn.forEach(Element =>{
+            Element.removeEventListener("click",btnClick(Element))} 
+        );
+        alert("You is winner!!!");
     }
-    else if(PcPoint > PlayerPoint){
-        return roundWinner = "PC win";
-    }
-    else{
-        return roundWinner = "DRAW";
+    else if(PcPoint >= 5) {
+        btn.forEach(Element =>{
+            Element.removeEventListener("click", event=>{
+                 btnClick(Element); 
+                })
+            } 
+        );
+        alert("Pc is winner!!!");
     }
 }
 
-const btn = document.querySelectorAll("button");
+const btn = document.querySelectorAll("button.btn");
+const handler = (PlayerPoint) =>PlayerPoint ++;
 
-btn.forEach(Element =>{
-    Element.addEventListener("click", event=>{
-         btnClick(Element); 
-        })
-    } 
-);
+btn.forEach(Element =>{Element.addEventListener("click", event=>{
+    btnClick(Element); 
+   })});
 
+console.log("btn[0].Element.value");
+
+var newDiv = document.createElement("div");
+
+function addCount(){
+    var mydiv = document.getElementsByClassName("wrap")[0];
+    var textNode = document.createTextNode("0  -  0");
+    newDiv.appendChild(textNode);
+    newDiv.classList.add("count");
+    mydiv.appendChild(newDiv);
+}
+
+addCount();
 
 function btnClick(Element){
     playerSelection = Element.value.toLowerCase();
     computerSelection = computerPlay();
-    game(playerSelection, computerSelection);
+    pcChoiceText.innerHTML = "Computer choice : "+ computerSelection.toUpperCase();
+    playRound(playerSelection, computerSelection);
     newDiv.innerHTML = PlayerPoint + "  -  " + PcPoint;
-    winner[0].innerHTML = roundWinner;
+    winner.style.color = winnerColor;
+    winner.innerHTML = roundWinner;
+    game();
 }
-var newDiv = document.createElement("div");
+const restartBtn = document.getElementById("restart");
 
-function addCount(){
-    var mydiv = document.getElementsByClassName("wrap");
-    var textNode = document.createTextNode("0  -  0");
-    newDiv.appendChild(textNode);
-    newDiv.classList.add("count");
-    mydiv[0].appendChild(newDiv);
-}
-
-addCount();
+restartBtn.addEventListener("click",function(){
+    window.location.reload();
+    return false;
+});
